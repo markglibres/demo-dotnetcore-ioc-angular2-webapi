67b9190ac5eb9fad244e19739904e59822fef953
Mark Gil Libres <markglibres@gmail.com>
2017-06-10 21:50:44 +1000

    added service layer proj

---
+++ src/NetCoreDemo.Service/Interface/IVehicleService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetCoreDemo
+{
+    public interface IVehicleService
+    {
+        Task<Vehicle> GetById(long Id);
+        Task<Vehicle> AddBikeAsync(string make, string model, string engine, int wheels, BodyType bodyType);
+        Task<Vehicle> EditBikeAsync(long id, string make, string model, string engine, int wheels, BodyType bodyType);
+        Task<Vehicle> AddCarAsync(string make, string model, string engine, int doors, int wheels, BodyType bodyType);
+        Task<Vehicle> EditCarAsync(long id, string make, string model, string engine, int doors, int wheels, BodyType bodyType);
+        Task<IEnumerable<Vehicle>> GetAllAsync();
+    }
+}
