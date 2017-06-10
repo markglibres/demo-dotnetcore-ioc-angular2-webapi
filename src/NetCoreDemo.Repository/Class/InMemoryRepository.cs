c052f9e298af01409e9823f8db6da722f30b7093
Mark Gil Libres <markglibres@gmail.com>
2017-06-10 21:50:10 +1000

    added repository layer proj

---
+++ src/NetCoreDemo.Repository/Class/InMemoryRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace NetCoreDemo
+{
+    public abstract class InMemoryRepository<T> : IRepository<T>
+        where T : BaseEntity
+    {
+        protected readonly DbContext _context;
+        protected readonly DbSet<T> _set;
+        public InMemoryRepository(DbContext context)
+        {
+            _context = context;
+            _set = _context.Set<T>();
+        }
+
+        public async Task<T> GetAsync<TKey>(TKey id)
+        {
+            return await _set.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _set.ToListAsync<T>();
+        }
+
+        public async Task<T> InsertAsync(T entity)
+        {
+            var result = await _set.AddAsync(entity);
+            return result.Entity;
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            await Task.Run(() => _context.Entry<T>(entity).State = EntityState.Modified);
+        }
+
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+
+    }
+}
---
+++ src/NetCoreDemo.Repository/Context/VehicleContext.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace NetCoreDemo
+{
+    public class VehicleContext : DbContext
+    {
+        public DbSet<Vehicle> Bikes { get; set; }
+        public VehicleContext(DbContextOptions<VehicleContext> options) : base(options)
+        {
+
+        }
+
+        private void ApplyDefaultDates()
+        {
+            //default values for date created and modified
+            var now = DateTime.Now;
+            var entries = this.ChangeTracker.Entries<IAuditableEntity>();
+            foreach (var entry in entries)
+            {
+                var entity = entry.Entity;
+                if (entry.State == EntityState.Added)
+                    entity.DateCreated = now;
+
+                entity.DateModified = now;
+
+            }
+
+            this.ChangeTracker.DetectChanges();
+        }
+
+        public override int SaveChanges()
+        {
+            ApplyDefaultDates();
+            return base.SaveChanges();
+        }
+
+        public override int SaveChanges(bool acceptAllChangesOnSuccess)
+        {
+            ApplyDefaultDates();
+            return base.SaveChanges(acceptAllChangesOnSuccess);
+        }
+
+        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            ApplyDefaultDates();
+            return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+        }
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default(CancellationToken))
+        {
+            ApplyDefaultDates();
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
